1e02a1fda3d942da1b1a4e111b185c914cb47242
Putu Widyantara Artanta Wibawa <putuwaw973@gmail.com>
2022-04-05 09:33:39 +0800

    Create .java file for c_burung_beo

---
+++ 03. Ekspresi dan Masukan atau Keluaran/C. Burung Beo/c_burung_beo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class c_burung_beo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        scan.close();
+        System.out.println(str);
+    }
+}
