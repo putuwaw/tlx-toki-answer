93cd97e37bd77fe3d3704b7572cd5692dc7214c2
Putu Widyantara Artanta Wibawa <putuwaw973@gmail.com>
2022-04-11 21:50:19 +0800

    Create .cpp file for d_bebek_untuk_teman

---
+++ 03. Ekspresi dan Masukan atau Keluaran/D. Bebek untuk Teman/d_bebek_untuk_teman.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int N, M;
+    cin >> N >> M;
+    cout << "masing-masing " << N / M << endl;
+    cout << "bersisa " << N % M << endl;
+    return 0;
+}
