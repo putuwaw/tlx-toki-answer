7b387cf06559d9664291c1f0ff6dbf6f48f51a60
Putu Widyantara Artanta Wibawa <putuwaw973@gmail.com>
2022-04-12 22:02:18 +0800

    Create .rs file for d_bebek_untuk_teman

---
+++ 03. Ekspresi dan Masukan atau Keluaran/D. Bebek untuk Teman/d_bebek_untuk_teman.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut reader = String::new();
+    std::io::stdin().read_line(&mut reader).expect("");
+    let mut substr_iter = reader.split_whitespace();
+    let mut next_num = || -> usize { substr_iter.next().expect("").parse().expect("") };
+    let a = next_num();
+    let b = next_num();
+    println!("masing-masing {}", a / b);
+    println!("bersisa {}", a % b);
+}
