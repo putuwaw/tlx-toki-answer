a295fe67ce1e743831ff04d9fde0a768130e61bf
Putu Widyantara Artanta Wibawa <putuwaw973@gmail.com>
2022-04-22 23:53:35 +0800

    Create .pas file for f_transpos_matriks

---
+++ 03. Ekspresi dan Masukan atau Keluaran/F. Transpos Matriks/f_transpos_matriks.pas
@@ -0,0 +1,11 @@
+program e_transpos_matriks;
+uses crt;
+var 
+  a,b,c,d,e,f,g,h,i : byte;
+begin
+  read(a,b,c,d,e,f,g,h,i);
+  writeln(a,' ',d,' ',g);
+  writeln(b,' ',e,' ',h);
+  writeln(c,' ',f,' ',i);
+  readln;
+end.
