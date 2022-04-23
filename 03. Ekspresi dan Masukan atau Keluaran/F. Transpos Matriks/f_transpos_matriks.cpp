2baf792675713ef61efef0606a48e887af56f6f9
Putu Widyantara Artanta Wibawa <putuwaw973@gmail.com>
2022-04-23 22:30:49 +0800

    Create .cpp file for f_transpos_matriks

---
+++ 03. Ekspresi dan Masukan atau Keluaran/F. Transpos Matriks/f_transpos_matriks.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b, c, d, e, f, g, h, i;
+    cin >> a >> b >> c >> d >> e >> f >> g >> h >> i;
+    cout << a << " " << d << " " << g << endl;
+    cout << b << " " << e << " " << h << endl;
+    cout << c << " " << f << " " << i << endl;
+    return 0;
+}
