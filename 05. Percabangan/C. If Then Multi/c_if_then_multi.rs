1654ba7111db22257bb730216880bac7cefb28a1
Putu Widyantara Artanta Wibawa <putuwaw973@gmail.com>
2022-05-12 19:57:00 +0800

    Create .rs file for c_if_then_multi

---
+++ 05. Percabangan/C. If Then Multi/c_if_then_multi.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut reader = String::new();
+    std::io::stdin().read_line(&mut reader).expect("");
+    let n: i32 = reader.trim().parse().expect("");
+    if n > 0 && n % 2 == 0 {
+        println!("{}", n);
+    }
+}
