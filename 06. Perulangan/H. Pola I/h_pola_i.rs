5837daaa47d135580eacca1b1ee5cee1bff71468
Putu Widyantara Artanta Wibawa <putuwaw973@gmail.com>
2022-07-06 15:29:57 +0800

    Create .rs file for h_pola_i

---
+++ 06. Perulangan/H. Pola I/h_pola_i.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let mut reader = String::new();
+    std::io::stdin().read_line(&mut reader).expect("");
+    let mut substr_iter = reader.split_whitespace();
+    let mut next_num = || -> i32 { substr_iter.next().expect("").parse().expect("") };
+    let n = next_num();
+    let k = next_num();
+    for i in 1..n + 1 {
+        if i % k == 0 {
+            print!("*");
+        } else {
+            print!("{}", i);
+        }
+        if i != n {
+            print!(" ");
+        } else {
+            println!("");
+        }
+    }
+}
