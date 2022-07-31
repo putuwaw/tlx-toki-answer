e4bb72b5f2548f1815f23500240946215ddf9cca
Putu Widyantara Artanta Wibawa <putuwaw973@gmail.com>
2022-07-31 21:59:56 +0800

    Create .java file for b_tukang_sulap

--- 10. Subprogram/B. Tukang Sulap/b_tukang_sulap.java
+++ 10. Subprogram/B. Tukang Sulap/b_tukang_sulap.java
@@ -8,19 +8,19 @@ class array {
 }
 
 class b_tukang_sulap {
-    public static void swap(array myarr, int P, int x, int Q, int y) {
-        int temp = myarr.arr[P][x];
-        myarr.arr[P][x] = myarr.arr[Q][y];
-        myarr.arr[Q][y] = temp;
+    public static void swap(array swapArr, int P, int x, int Q, int y) {
+        int temp = swapArr.arr[P][x];
+        swapArr.arr[P][x] = swapArr.arr[Q][y];
+        swapArr.arr[Q][y] = temp;
     }
     public static void main(String[] args) {
-        array myarr = new array();
+        array swapArr = new array();
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < N; j++) {
                 int num = scan.nextInt();
-                myarr.arr[i][j] = num;
+                swapArr.arr[i][j] = num;
             }
         }
         int T = scan.nextInt();
@@ -33,12 +33,12 @@ class b_tukang_sulap {
             y = scan.nextInt();
             p1 = P - 'A';
             q1 = Q - 'A';
-            swap(myarr, p1, x - 1, q1, y - 1);
+            swap(swapArr, p1, x - 1, q1, y - 1);
         }
         scan.close();
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < N; j++) {
-                System.out.print(myarr.arr[i][j] + " ");
+                System.out.print(swapArr.arr[i][j] + " ");
             }
             System.out.println();
         }
