9790ddd39ebe126d2845ca6300656e055cd4c772
Putu Widyantara Artanta Wibawa <putuwaw973@gmail.com>
2022-08-07 13:02:12 +0800

    Create .c file for f_teman_dekat

---
+++ 10. Subprogram/F. Teman Dekat/f_teman_dekat.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int power(int base, int exp){
+  int res = base;
+  while (exp > 1){
+    res *= base;
+    exp--;
+  }
+  return res;
+}
+
+int main(){
+  int N, D, i, j, res, min, max;
+  scanf("%d %d", &N, &D);
+  int T[N][2];
+  for (i = 0; i < N; i++)
+    for (j = 0; j < 2; j++)
+      scanf("%d", &T[i][j]);
+
+  for (i = 0; i < (N - 1); i++){
+    for (j = i + 1; j < N; j++){
+      res = power(abs(T[j][0] - T[i][0]), D) + power(abs(T[j][1] - T[i][1]), D);
+      if (i == 0 && j == 1){
+        min = max = res;
+      }
+      else{
+        if (res < min) min = res;
+        if (res > max) max = res;
+      }
+    }
+  }
+  printf("%d %d\n", min, max);
+  return 0;
+}
